85e3171b10b19b5242a54b207b6b4a4adfe2f875
Jackie Luc <jackieluc17@gmail.com>
2019-07-31 20:40:58 -0600

    Add Sunshiney typography

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,8 +1,18 @@
+const path = require("path")
+
 module.exports = () => ({
   siteMetadata: {
     title: "Gatsby Theme 90s",
     author: `Jackie Luc`,
     description: `gatsby-theme-90s is a Gatsby Theme with some flair from the old web.`,
   },
-  plugins: ["gatsby-plugin-theme-ui"],
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: path.resolve(`${__dirname}/src/utils/typography`),
+      },
+    },
+  ],
 })
---
+++ theme/src/utils/typography.js
@@ -0,0 +1,13 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  googleFonts: [
+    {
+      name: "Sunshiney",
+      styles: ["400"],
+    },
+  ],
+})
+
+export default typography
